514202fb83270cddfcbc102edc332600316f9bdf
Tyson Williams <34664007+TysonMN@users.noreply.github.com>
2021-03-19 10:17:33 -0500

    DialogHostContent is Visible every time

---
+++ Project/App.xaml.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows;
+
+namespace WpfApp2 {
+  public partial class App : Application { }
+}
---
+++ Project/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WpfApp2 {
+  public partial class MainWindow : Window, INotifyPropertyChanged {
+
+    public MainWindow() {
+      DataContext = this;
+      InitializeComponent();
+    }
+
+    private Visibility dialogHostContextTextVisibility = Visibility.Collapsed;
+    public Visibility DialogHostContextTextVisibility {
+      get => dialogHostContextTextVisibility;
+      set {
+        dialogHostContextTextVisibility = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private bool isDialogOpen = false;
+    public bool IsDialogOpen {
+      get => isDialogOpen;
+      set {
+        isDialogOpen = value;
+        DialogHostContextTextVisibility = value ? Visibility.Visible : Visibility.Collapsed;
+        OnPropertyChanged();
+      }
+    }
+
+    private void OpenDialog(object _1, RoutedEventArgs _2) =>
+      IsDialogOpen = true;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+  }
+}
